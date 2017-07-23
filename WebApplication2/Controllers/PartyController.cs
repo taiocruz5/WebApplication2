0a351b56953c67d37ec28858c56ef0fe5a9cfd4d
ho shu heng <ho shu heng>
2017-07-24 00:01:58 +0800

    Add project files.

---
+++ WebApplication2/Controllers/PartyController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
+
+namespace PartyInvites.Controllers
+{
+    public class PartyController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+        [HttpGet]
+        public ViewResult RSVPForm(){
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RSVPForm(GuestResponse guestresponse) {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestresponse);
+                return View("Thanks", guestresponse);
+            }
+            else
+            {
+                //there is a validation error
+                return View();
+
+            }
+        }
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication2/Services/AuthMessageSenderOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication2.Services
+{
+
+
+
+    public partial class AuthMessageSender : IEmailSender, ISmsSender
+    {
+        public class AuthMessageSenderOptions
+        {
+            public string SendGridUser { get; set; }
+            public string SendGridKey { get; set; }
+        }
+    }
+}
